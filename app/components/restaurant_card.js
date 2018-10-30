4e6df0fbf003598a3bd84fc2cdbc70456be1d10d
fujianto <septianfujianto@gmail.com>
2018-10-30 19:40:56 +0700

    Clean card component

--- app/components/restaurant_card.js
+++ app/components/restaurant_card.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { Image, View } from 'react-native';
+import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, H3 } from 'native-base';
 
-export const RestaurantCard = () => {
+const Styles = {
+  featuredWrapper: { 
+    position: 'relative', height: 150, flex: 1 
+  },
+  featuredImage: { 
+    position: 'absolute', 
+    top: 0, 
+    left: 0, 
+    bottom: 0, 
+    right: 0
+  }
+}
+
+export const RestaurantCard = (props) => {
   return (
     <Card style={{ flex: 0 }}>
       <CardItem>
-        <Left>
-          <Thumbnail source={{ uri: 'http://naisinpo.com/wp-content/uploads/2018/04/IMG-20180419-WA0000-150x150.jpg' }} />
-          <Body>
-            <Text>NativeBase</Text>
-            <Text note>April 15, 2016</Text>
-          </Body>
-        </Left>
-      </CardItem>
-      <CardItem>
         <Body>
-          <Image source={{ uri: 'http://naisinpo.com/wp-content/uploads/2018/04/IMG-20180419-WA0000-150x150.jpg' }} style={{ height: 200, width: 200, flex: 1 }} />
-          <Text>
-          //Your text here
-          </Text>
+          <View styles={{ position: 'relative', height: 150, flex: 1}}>
+            <Image resizeMode="cover" style={{
+              position: 'absolute', 
+              height: 200, width: 200, flex: 1
+            }} source={{ uri: 'http://naisinpo.com/wp-content/uploads/2018/04/IMG-20180419-WA0000-150x150.jpg' }} />
+          </View>
+          <H3>Name of Restaurant</H3>
         </Body>
-      </CardItem>
-      <CardItem>
-        <Left>
-          <Button transparent textStyle={{ color: '#87838B' }}>
-            <Icon name="logo-github" />
-            <Text>1,926 stars</Text>
-          </Button>
-        </Left>
       </CardItem>
     </Card>
   );
